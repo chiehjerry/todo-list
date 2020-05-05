793d85e4344e54db6952d588bf0a48d965a91143
chiehjerry <chiehjerry@gmail.com>
2020-05-05 09:30:05 +0800

    feat:add todo seeder

---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,19 @@
+//載入 mongoose
+const mongoose = require('mongoose')
+const Todo = require('../todo') // 載入 todo model
+// 載入mongoose並設定連線，
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: 'name-' + i })
+  }
+  console.log('done')
+})
