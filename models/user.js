00f24ec66da5b22c9210ca89fb7f75173fc0d8e0
chiehjerry <chiehjerry@gmail.com>
2020-05-25 13:57:58 +0800

    Add user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+//載入 mongoose
+const mongoose = require('mongoose')
+//mongoose.Schema模組
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+module.exports = mongoose.model('User', userSchema)
