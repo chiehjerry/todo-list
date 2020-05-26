9f841373b02cf57ed5beaea392c8ffb815f10c00
chiehjerry <chiehjerry@gmail.com>
2020-05-26 11:29:50 +0800

    Add user authentication in todo and home routers

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,9 +3,12 @@ const express = require('express')
 const router = express.Router()
 // 引用 Todo model
 const Todo = require('../../models/todo')
-// 定義首頁路由
+// 載入 auth middleware 裡的 authenticated 方法
+const { authenticated } = require('../../config/auth')
 
-router.get('/', (req, res) => {
+
+// 定義首頁路由
+router.get('/', authenticated, (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .sort({ _id: 'asc' }) // desc
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -1,30 +1,32 @@
 const express = require('express')
 const router = express.Router()
 const Todo = require('../../models/todo')
-router.get('/new', (req, res) => {
+const { authenticated } = require('../../config/auth')
+
+router.get('/new', authenticated, (req, res) => {
   return res.render('new')
 })
-router.post('/', (req, res) => {
+router.post('/', authenticated, (req, res) => {
   const name = req.body.name
   return Todo.create({ name })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
-router.get('/:id', (req, res) => {
+router.get('/:id', authenticated, (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .lean()
     .then(todo => res.render('detail', { todo }))
     .catch(error => console.log(error))
 })
-router.get('/:id/edit', (req, res) => {
+router.get('/:id/edit', authenticated, (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .lean()
     .then(todo => res.render('edit', { todo }))
     .catch(error => console.log(error))
 })
-router.put('/:id', (req, res) => {
+router.put('/:id', authenticated, (req, res) => {
   const id = req.params.id
   const { name, isDone } = req.body
   return Todo.findById(id)
@@ -36,7 +38,7 @@ router.put('/:id', (req, res) => {
     .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
-router.delete('/:id', (req, res) => {
+router.delete('/:id', authenticated, (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
     .then(todo => todo.remove())
